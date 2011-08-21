7da155f53fd1a0cf99e3c7df412838f3a67d1d96
Samuel Charron <samuel.charron@gmail.com>
2011-08-22 01:13:00 +0200

    Add about.php page

---
+++ about.php
@@ -0,0 +1,81 @@
+<!doctype html>
+<html lang="fr">
+<head>
+	<meta charset="utf-8">
+	<title>Post-It War Generator</title>
+	<link href="favicon.ico" type="image/x-icon" rel="icon" />
+	<link rel="stylesheet" type="text/css" href="r/css/g.css" />
+	<link rel="stylesheet" type="text/css" href="r/css/about.css" />
+	<script type="text/javascript" src="r/js/jq1.6.2.js"></script>
+	<script type="text/javascript" src="r/js/jq.ui.1.8.15.js"></script>
+	<script type="text/javascript" src="r/js/farbtastic.js"></script>
+	<meta name="Description" content="A propos du post-it générator">
+	<meta name="Robots" content="all">
+	<meta name="Keywords" content="postit, war, générator, générateur, post, it, propos">
+	<!--[if lt IE 9]>
+	<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+
+	<?php include "r/php/jsForGG.php"; ?>
+	
+</head>
+<body>
+
+	<div class="container">
+	<?php include "r/php/ttl.php"; ?>
+	
+		<img class="example" src="r/panda.png" width="768" height="343" alt="Exemple d'application concréte du générateur" />
+		
+		<p>
+			<a href="http://www.lavoixeco.com/actualite/Secteurs_activites/Commerces_et_Distribution/2011/08/20/article_guerre-des-post-it-apres-les-fenetres-qu.shtml">Phénomène à la mode</a>, 
+			la post-it war consiste a coller sur les fenêtres (ou mur) de son entreprise des motifs de post-it, 
+			et de surenchérir par rapport a ses voisins !
+		</p>
+		<p>
+			Le <b>post-it war générator</b> est l'outil qui va vous permettre de remporter cette guerre haut la main !
+			Choisissez le logo ou le pixel art de votre choix et transformer le automatiquement en une mosaïque de post-it aux dimensions concrétement applicable facilement !
+		</p>
+		<p>	
+			Personne n'est oublié dans le monde de l'entreprise, car vous pourrez 
+		</p>
+		<p>
+			Evidemment, ce serait présomptueux d'affirmer que ce service est parfait... Aussi, 
+			voici les questions que vous pouvez vous posez, et leur réponses :
+		</p>
+		
+		<ul>
+			<li>
+				<span class="ask">Mon image est toute blanche ?</span>
+				<span class="answer">
+					C'est probablement que la couleur de vos post-it étaient trop clairs, et
+					que ceux-ci n'ont pas put reconnaitre les zones sombres de votre image qui ne devaient
+					présenter que des zones sombres. Notre algorithme est imparfait mais la vie est ainsi !
+				</span>
+			</li>
+			<li>
+				<span class="ask">Comment faire pour avoir un rendu nickel ?</span>
+				<span class="answer">
+					Choisissez des images petites, aux tons contrastés, et de préférences avec des post-its aux
+					couleurs proches :)
+				</span>
+			</li>
+			<li>
+				<span class="ask">Le site rame abominablement !</span>
+				<span class="answer">
+					C'est vrai. Plus vous mettrez une dimension monstrueuse, plus il ramera :s
+				</span>
+			</li>
+			<li>
+				<span class="ask">Et si je veux poser une question qui n'est pas présente dans la FAQ ?</span>
+				<span class="answer">
+					Envoyez moi un e-mail : <a href="mailto:webmaster@simonertel.net">webmaster@simonertel.net</a> :)
+				</span>
+			</li>
+
+		</ul>
+		
+	<?php include "r/php/footer.php"; ?>
+	</div>
+
+</body>
+</html>
