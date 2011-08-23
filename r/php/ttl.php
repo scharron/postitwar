d8bc110fd2cbc4ddcc9cb6fb2766d5570a55e0b8
Latouss <lt.laurent.toussaint@gmail.com>
2011-08-23 23:28:24 +0200

    Add mink to change lang

--- r/php/ttl.php
+++ r/php/ttl.php
@@ -1,9 +1,26 @@
+<?php
+
+$lang = array(
+	'fr' => 'Français',
+	'en' => 'English'
+);
+
+?>
 <h1><a title="revenir sur la HomePage" href="index.php">Post-it Générator !</a></h1>
 <div class="subttl">
 
 	<h2>Transformer n'importe quelle image en un festival de Post-It !</h2>
 	
 	<div class="sn">
+		<div style="float:right;">
+			<?php
+				foreach($lang as $iso => $text)
+				{
+					echo '<a href="r/changeLang.php?lang=' . $iso . '" class="lang_link ' . ( $lang == $iso ? ' lang_selected' : '' ) . '">' . $text . '</a> ';
+				}
+			?>
+		</div>
+		
 		<g:plusone size="small"></g:plusone>
 		<a href="http://twitter.com/share" class="twitter-share-button" data-url="http://postitwar.me" data-count="none" data-lang="fr">Tweeter</a>
 		<div id="fb-root"></div>
