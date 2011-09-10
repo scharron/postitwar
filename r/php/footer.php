383b8dda934ad6bf4b118d1ca999300cf99c9cb4
Latouss <lt.laurent.toussaint@gmail.com>
2011-09-11 01:41:57 +0200

    Correction

--- r/php/footer.php
+++ r/php/footer.php
@@ -1,6 +1,6 @@
 <footer>
 	{tr id=ALGO_DEV}Algorithme/Développement{/tr} > <a target="_blank" href="http://www.samuelcharron.fr">Samuel Charron</a> | 
 	{tr id=DESIGN_INTERFACE}Design/Interface javascript{/tr} > <a target="_blank" href="http://simonertel.net">Simon Ertel</a> |
-	{tr id=TRAD_DEV}Systéme de traduction{/tr} > <a target="_blank" href="http://www.viadeo.com/profile/002g3hgg4xsd048">Laurent Toussaint</a> | 
+	{tr id=TRAD_DEV}Système de traduction{/tr} > <a target="_blank" href="http://www.viadeo.com/profile/002g3hgg4xsd048">Laurent Toussaint</a> | 
 	<a href="about.php">{tr id=ABOUT_FAQ}A propos / F.A.Q.{/tr}</a> | 
 </footer>
